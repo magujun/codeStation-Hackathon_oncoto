909dd08b2b7a3c97db71ee5d40c9b31929a6ce62
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-17 17:30:11 -0700

    Basic Setup of ContextProvider.

--- frontend/src/context/GameContext.tsx
+++ frontend/src/context/GameContext.tsx
@@ -1,20 +1,56 @@
-import { createContext } from 'react';
+import { createContext, ReactNode, useCallback, useState } from 'react';
 
 type Answers = {
   correctAnswer: string;
   userAnswer: string;
 }
 
-interface IGameContext {
+export interface GameContextData {
   answers: Answers;
   startTime: string;
   endTime: string;
   difficultyLevel: string;
   handleStartNewGame: () => void;
-  handleEndNewGame: () => void;
+  handleEndGame: () => void;
   handleSubmitAnswer: () => void;
 }
 
-export const GameContext = createContext({} as IGameContext);
+interface GameProviderProps {
+  children: ReactNode;
+}
 
+export const GameContext = createContext({} as GameContextData);
+
+export function GameProvider({ children }) {
+  const [answers, setAnswers] = useState<Answers>(null);
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+  const [difficultyLevel, setDifficultyLevel] = useState('');
+
+  const handleStartNewGame = useCallback(() => {
+    // TODO
+  }, []);
+
+  const handleEndGame = useCallback(() => {
+    // TODO
+  }, []);
+
+  const handleSubmitAnswer = useCallback(() => {
+    // TODO
+  }, []);
+
+  return (
+    <GameContext.Provider value={{
+      answers,
+      difficultyLevel,
+      endTime,
+      startTime,
+      handleStartNewGame,
+      handleEndGame,
+      handleSubmitAnswer
+    }}>
+      {children}
+    </GameContext.Provider>
+  );
+}
 
