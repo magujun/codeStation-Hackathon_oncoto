9c93f7b07e1cef4ba79b94a29494481a36813de3
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-17 23:22:38 -0700

    Renamed getTimeInterval to getFormatedTimeInterval.

--- frontend/src/utils/getFormatedTimeInterval.spec.ts
+++ frontend/src/utils/getFormatedTimeInterval.spec.ts
@@ -1,18 +1,18 @@
-import { getTimeInterval } from './getTimeInterval';
+import { getFormatedTimeInterval } from './getFormatedTimeInterval';
 import { mocked } from 'ts-jest/utils';
 import { differenceInSeconds } from 'date-fns';
 
 jest.mock('date-fns');
 
-describe('getTimeInterval utility function', () => {
-  it('should ', () => {
+describe('getFormatedTimeInterval utility function', () => {
+  it('should return a formated string with the time interval', () => {
     const mockedDifferenceInSeconds = mocked(differenceInSeconds);
     mockedDifferenceInSeconds.mockReturnValueOnce(90);
 
     const fakeStartDate = new Date(2021, 5, 17, 0, 0, 0);
     const fakeEndDate = new Date(2021, 5, 17, 0, 1, 5);
 
-    const lapsedTime = getTimeInterval(fakeStartDate, fakeEndDate);
+    const lapsedTime = getFormatedTimeInterval(fakeStartDate, fakeEndDate);
 
     expect(lapsedTime).toBe('01:30');
   })
