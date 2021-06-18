0cd0b3f6344ac8523d3b35d9b3f6578f1af53974
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-17 23:31:56 -0700

    fixed test description typo.

--- frontend/src/utils/getFormatedElapsedTime.spec.ts
+++ frontend/src/utils/getFormatedElapsedTime.spec.ts
@@ -5,15 +5,15 @@ import { differenceInSeconds } from 'date-fns';
 jest.mock('date-fns');
 
 describe('getFormatedElapsedTime utility function', () => {
-  it('should return a formated string with the time interval', () => {
+  it('should return a formated string with the elapsed time', () => {
     const mockedDifferenceInSeconds = mocked(differenceInSeconds);
     mockedDifferenceInSeconds.mockReturnValueOnce(90);
 
     const fakeStartDate = new Date(2021, 5, 17, 0, 0, 0);
     const fakeEndDate = new Date(2021, 5, 17, 0, 1, 5);
 
-    const lapsedTime = getFormatedElapsedTime(fakeStartDate, fakeEndDate);
+    const elapsedTime = getFormatedElapsedTime(fakeStartDate, fakeEndDate);
 
-    expect(lapsedTime).toBe('01:30');
+    expect(elapsedTime).toBe('01:30');
   })
 })
