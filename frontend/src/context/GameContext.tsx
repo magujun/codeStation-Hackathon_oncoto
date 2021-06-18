164b7cf8a81c0aced29a03e1bf1b162e8c7c964c
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-17 17:19:56 -0700

    Created Game Context.

---
+++ frontend/src/context/GameContext.tsx
@@ -0,0 +1,18 @@
+import { createContext } from 'react';
+
+type Answers = {
+  correctAnswer: string;
+  userAnswer: string;
+}
+
+interface IGameContext {
+  answers: Answers;
+  startTime: string;
+  endTime: string;
+  difficultyLevel: string;
+  handleStartNewGame: () => void;
+  handleEndNewGame: () => void;
+  handleSubmitAnswer: () => void;
+}
+
+export const GameContext = createContext({} as IGameContext);
