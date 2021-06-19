7ed9188124ef272e82c07b297b15ff1e3c10d8cc
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-06-19 03:56:14 -0300

    Updating env variable FRONTEND_AUTH_KEY to NEXT_PUBLIC_FRONTEND_AUTH_KEY

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_API,
   headers: {
-    Authorization: process.env.FRONTEND_AUTH_KEY,
+    Authorization: process.env.NEXT_PUBLIC_FRONTEND_AUTH_KEY,
   }
 });
