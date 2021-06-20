a38933276892d5d4cf6e65cb9147ae2ca8be9448
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-20 01:01:04 -0700

    Made Elements more responsive.

--- frontend/src/pages/games/summary/index.tsx
+++ frontend/src/pages/games/summary/index.tsx
@@ -5,6 +5,7 @@ import Router from 'next/router';
 import { GetServerSideProps } from 'next';
 import {
   Box,
+  Flex,
   Grid,
   Stack,
   Text,
@@ -71,13 +72,13 @@ const Summary = ({ googleMapsApiKey }: SummaryProps) => {
           >
             <Stack display="flex" alignItems="center" justifyContent="center">
               <Text fontSize="1.5rem">Sua pontuação</Text>
-              <Text fontSize="6rem" fontWeight="bold">
+              <Text fontSize={{ base: "4rem", md: "5rem" }} fontWeight="bold">
                 {score < 0 ? 0 : score}
               </Text>
             </Stack>
             <Stack display="flex" alignItems="center" justifyContent="center">
               <Text fontSize="1.5rem">Tempo</Text>
-              <Text fontSize="6rem" fontWeight="bold">
+              <Text fontSize={{ base: "4rem", md: "5rem" }} fontWeight="bold">
                 {elapsedTime ?? ''}
               </Text>
             </Stack>
@@ -87,7 +88,7 @@ const Summary = ({ googleMapsApiKey }: SummaryProps) => {
               justifyContent="space-between"
             >
               <Text fontSize="1.5rem">Distância</Text>
-              <Text fontSize="6rem" fontWeight="bold">
+              <Text fontSize={{ base: "4rem", md: "5rem" }} fontWeight="bold">
                 {distance < 0
                   ? ''
                   : distanceInKM > 1800
@@ -96,40 +97,41 @@ const Summary = ({ googleMapsApiKey }: SummaryProps) => {
               </Text>
             </Stack>
           </Grid>
-          <Box
+          <Flex
             mt="10"
+            mb={{ base: '10', md: '0' }}
             mx="auto"
             w={{ base: '100%', md: '600px' }}
             height="250px"
-            display="flex"
             flexDir="column"
             alignContent="center"
             justifyContent="center"
-            bg="white"
             borderRadius="30px"
+            backgroundColor="rgba(255, 255, 255, 0.85)"
+            boxShadow="4px 10px 30px rgba(159, 209, 255, 0.1);"
           >
-            <Box p="100px" textAlign="center">
-              <Text fontWeight="bold" fontSize="1.75rem" mb="4">
+            <Box p={{ base: "2.25rem", md: "6.25rem" }} textAlign="center">
+              <Text fontWeight="bold" fontSize={{ base: "lg", sm: "xl", md: "2xl" }} mb="4">
                 Deseja jogar novamente?
               </Text>
-              <Box w="100%" display="flex">
+              <Flex w="100%" alignItems="center" flexDirection={{ base: 'column', md: 'row' }}>
                 <OutlinedButton
-                  m="1"
-                  flex="1"
+                  mr={{ base: '0', md: '1' }}
+                  mb={{ base: '2', md: '0' }}
+                  w="100%"
                   onClick={handleRedirectUserToDashboard}
                 >
                   Não
                 </OutlinedButton>
                 <Button
-                  m="1"
-                  flex="1"
+                  w="100%"
                   onClick={onOpen}
                 >
                   Sim
                 </Button>
-              </Box>
+              </Flex>
             </Box>
-          </Box>
+          </Flex>
         </Container>
       </main>
       <Modal open={isOpen} onClose={onClose}>
