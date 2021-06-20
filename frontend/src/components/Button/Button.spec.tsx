6e29e132c890e8bbe8ff4dbe5f4672b6996cf624
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-19 18:44:33 -0700

    Extracted outlined button from regular button component.

--- frontend/src/components/Button/Button.spec.tsx
+++ frontend/src/components/Button/Button.spec.tsx
@@ -1,28 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import { Button } from '.';
-import { FaGlobe } from 'react-icons/fa';
 
 describe('Button component', () => {
-  it('should render a primary button as default', () => {
+  it('should render the solid button correctly', () => {
     render(<Button>Test</Button>)
 
     const testButton = screen.getByText('Test');
 
     expect(testButton).toBeInTheDocument();
     expect(testButton).not.toHaveStyle('background-color: transparent');
-  });
-
-  it('should render a secondary button with transparent backgound if isPrimary is set to false', () => {
-    render(<Button isPrimary={false}>Test</Button>)
-
-    const testButton = screen.getByText('Test');
-
-    expect(testButton).toHaveStyle('background-color: transparent');
-  });
-
-  it('should render an icon if props is passed', () => {
-    render(<Button icon={FaGlobe}>Test</Button>)
-
-    expect(screen.getByTestId('button-icon')).toBeInTheDocument();
   });
 })
