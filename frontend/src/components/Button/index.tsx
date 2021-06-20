1c5fb0da3ca0448e7e0be1c7ce981f6097bc09a1
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-19 17:51:13 -0700

    Removed unnecessary icon props in button.

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,18 +1,16 @@
-import { Button as ChakraUIButton, ButtonProps as ChakraUIButtonProps, Icon as ChakraUIIcon } from "@chakra-ui/react";
+import { Button as ChakraUIButton, ButtonProps as ChakraUIButtonProps } from "@chakra-ui/react";
 import { ReactNode, memo } from "react";
-import { IconBaseProps } from "react-icons";
 
 interface ButtonProps extends ChakraUIButtonProps {
   children: ReactNode;
   isPrimary?: boolean;
-  icon?: React.ComponentType<IconBaseProps>;
 }
 
-export const Button = memo(({ children, icon: Icon, isPrimary = true, ...rest }: ButtonProps) => {
+export const Button = memo(({ children, isPrimary = true, ...rest }: ButtonProps) => {
   if (!isPrimary) {
     return (
       <ChakraUIButton
-        variant="unstyled"
+        variant="outline"
         bg="transparent"
         color="blue.900"
         borderColor="blue.900"
@@ -24,14 +22,13 @@ export const Button = memo(({ children, icon: Icon, isPrimary = true, ...rest }:
         fontWeight="bold"
         {...rest}
       >
-        {Icon && (<ChakraUIIcon as={Icon} fontSize={{ base: "1rem", lg: "1.25rem" }} data-testid="button-icon" />)}
         {children}
       </ChakraUIButton>
     );
   }
   return (
     <ChakraUIButton
-      variant="unstyled"
+      variant="solid"
       bg="blue.900"
       color="white"
       _hover={{ filter: 'brightness(1.1)' }}
@@ -41,7 +38,6 @@ export const Button = memo(({ children, icon: Icon, isPrimary = true, ...rest }:
       fontWeight="bold"
       {...rest}
     >
-      {Icon && (<ChakraUIIcon as={Icon} fontSize={{ base: "1rem", lg: "1.25rem" }} data-testid="button-icon" />)}
       {children}
     </ChakraUIButton>
   );
