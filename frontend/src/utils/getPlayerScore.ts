a78cdc8326c92c0e1ba9a234507153a4dce1c5fd
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-06-20 11:30:08 -0300

    Fixxing score rules

--- frontend/src/utils/getPlayerScore.ts
+++ frontend/src/utils/getPlayerScore.ts
@@ -1,19 +1,23 @@
 import { getLevelTime } from './getLevelTime';
 type DifficultyLevel = 'easy' | 'medium' | 'hard';
 
-export const getPlayerScore = (distance: number, level: DifficultyLevel, elapseTime: number) => {
-
+export const getPlayerScore = (
+  distance: number,
+  level: DifficultyLevel,
+  elapseTime: number,
+) => {
   const totalTime = getLevelTime(level);
   const distanceInKm = distance / 1000;
-  const percentNotUsed = totalTime <= 0 ? 0 : (100 - ((elapseTime * 1000) * 100 / totalTime)) / 100;
+  const percentNotUsed =
+    totalTime <= 0 ? 0 : (100 - (elapseTime * 1000 * 100) / totalTime) / 100;
 
   const levelBonus = {
     easy: 0.3,
     medium: 0.6,
     hard: 1,
-  }
+  };
 
-  const bonus = 1 + (levelBonus[level] * percentNotUsed);
+  const bonus = 1 + levelBonus[level] * percentNotUsed;
 
   var score = 0;
 
@@ -21,29 +25,26 @@ export const getPlayerScore = (distance: number, level: DifficultyLevel, elapseT
 
   console.log('get player score', distanceInKm);
 
-  if (distanceInKm >= 0 && distanceInKm <= 10) score += 800;
+  if (distance <= 100) {
+    score = 1000;
+  } else if (distanceInKm <= 1) {
+    score = 900;
+  } else if (distanceInKm <= 10) {
+    score = 850;
+  } else if (distanceInKm <= 100) {
+    score = 700;
+  } else if (distanceInKm <= 500) {
+    score = 500;
+  } else if (distanceInKm > 1000) {
+    score = 100;
+  }
 
-  if (distanceInKm > 10 && distanceInKm <= 100) score += 600;
 
-  if (distanceInKm > 100 && distanceInKm <= 400) score += 500;
+  const finalScore = Math.floor(score * bonus);
 
-  if (distanceInKm > 400 && distanceInKm <= 800) score += 400;
+  if (isNaN(finalScore)) return 0;
 
-  if (distanceInKm > 800 && distanceInKm <= 1000) score += 300;
+  if (finalScore > 1000) return 1000;
 
-  if (distanceInKm > 1000 && distanceInKm <= 10000) score += 200;
-
-  if (distanceInKm > 10000 && distanceInKm <= 10000000) score += 25;
-
-  if (distanceInKm > 10000000 && distanceInKm <= 100000000) score += 15;
-
-  score += 3;
-
-  score *= bonus;
-
-  if (isNaN(Math.floor(score))) return 50;
-
-  if (score > 1000) return 1000;
-
-  return Math.floor(score);
-}
+  return finalScore;
+};
